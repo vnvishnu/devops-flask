aeb1f6832aa239f3242879b360a3f8ed4a0be932
vnvishnu <48054183+vnvishnu@users.noreply.github.com>
2022-07-03 17:55:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 def hello():
   return "<h1><center>Hello World app! Version 2</center><h1>"
 import socket
-print socket.getfqdn()
-if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=5000)
+def get_Host_name_IP():
+	try:
+		host_name = socket.gethostname()
+		host_ip = socket.gethostbyname(host_name)
+		print("Hostname : ",host_name)
+		print("IP : ",host_ip)
+	except:
+		print("Unable to get Hostname and IP"
+app.run(host='0.0.0.0',port=5000)
